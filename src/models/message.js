947018150eea760d31ffc29e892fcaa0d5935996
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-04-19 16:03:57 -0700

    Add files via upload

---
+++ src/models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Add your models here.
+const MessageSchema = new Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    author: { type:Schema.Types.ObjectId, ref:"User", required: true },
+})
+
+const message = mongoose.model('Message', MessageSchema)
